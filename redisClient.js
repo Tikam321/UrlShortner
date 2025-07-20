27f61aa79cd91b356faabb73a764971b88ae1e4b
tikam321 <tikamsuvasiya333@gmail.com>
2025-07-20 18:07:20 +0530

    added the dockerfile and docker-compose for docker builder

--- redisClient.js
+++ redisClient.js
@@ -1,6 +1,11 @@
 const { createClient } = require("redis");
 
-const client = createClient();
+// const client = createClient();
+
+// docker container confirguration 
+const client = createClient({
+  url: 'redis://redis:6379'
+});
 
 client.on("error", (err) => {
   console.error("Redis Client Error", err);
